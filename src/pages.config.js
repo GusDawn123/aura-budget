622bb9e3f05925d7167bec79fa320760f030d835
Gustavo Rosas <gustavo@launchpadacquisition.com>
2026-01-16 22:08:46 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,13 +1,7 @@
-import Dashboard from './pages/Dashboard';
-import Transactions from './pages/Transactions';
-import Bills from './pages/Bills';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Dashboard": Dashboard,
-    "Transactions": Transactions,
-    "Bills": Bills,
 }
 
 export const pagesConfig = {
