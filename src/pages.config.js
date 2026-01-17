4ed70e916fff72ab92a337389b4eca62c899fb00
Gustavo Rosas <gustavo@launchpadacquisition.com>
2026-01-17 00:13:33 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,23 +1,9 @@
-import Overview from './pages/Overview';
-import MoneyIn from './pages/MoneyIn';
-import MoneyOut from './pages/MoneyOut';
-import Bills from './pages/Bills';
-import Snapshot from './pages/Snapshot';
-import Settings from './pages/Settings';
-import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Overview": Overview,
-    "MoneyIn": MoneyIn,
-    "MoneyOut": MoneyOut,
-    "Bills": Bills,
-    "Snapshot": Snapshot,
-    "Settings": Settings,
 }
 
 export const pagesConfig = {
     mainPage: "Overview",
     Pages: PAGES,
-    Layout: __Layout,
 };
